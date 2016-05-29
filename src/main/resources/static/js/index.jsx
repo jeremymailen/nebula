fb013ddec43c7aebd95c6913845595ab6fffd4f7
Jeremy Mailen <jeremy@gladly.com>
2016-05-29 08:30:55 -0700

    Scafolding for React SPA

---
+++ src/main/resources/static/js/index.jsx
@@ -0,0 +1,12 @@
+var AppContainer = React.createClass({
+    render: function() {
+        return (
+            <div>App</div>
+        );
+    }
+});
+
+ReactDOM.render(
+  <AppContainer />,
+  document.getElementById('app')
+);
